dc97168abcd85fe06bf37fd3e46824255f950035
Jieun <jien617@naver.com>
2020-11-18 14:31:58 +0900

    service_comm

--- algorithm/script/algorithm_subscriber.py
+++ algorithm/script/algorithm_subscriber.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import rospy
 from common_msgs.msg import common_msgs
+from common_msgs.srv import AddTwoNum, AddTwoNumResponse
+
 
 def callback(msg):
-    print "subscribe:", msg.timestamp.secs%100, msg.vector.x, msg.vector.y, msg.vector.z
+    print("subscribe:", msg.timestamp.secs%100, msg.vector.x, msg.vector.y, msg.vector.z)
 
-rospy.init_node('algorithm_subscriber')
-sub = rospy.Subscriber('algorithm_msg', common_msgs, callback)
+def service_callback(request):
+    response = AddTwoNumResponse(sum=request.a + request.b + request.c)
+    print("request data:", request.a, request.b, request.c, " response:", response.sum)
+    return response
+
+
+rospy.init_node('algorithm_service')
+sub = rospy.Subscriber('custom_msg', common_msgs, callback)
+service = rospy.Service('add_two_number', AddTwoNum, service_callback)
 rospy.spin()
--- sensor/script/sensor_publisher.py
+++ sensor/script/sensor_publisher.py
@@ -2,16 +2,23 @@
 import rospy
 from geometry_msgs.msg import Vector3
 from common_msgs.msg import common_msgs
+from common_msgs.srv import AddTwoNum, AddTwoNumRequest
 
-rospy.init_node('sensor_publisher')
-pub = rospy.Publisher('sensor_msg', common_msgs, queue_size=1)
+
+rospy.init_node('sensor_service')
+pub = rospy.Publisher('custom_msg', common_msgs, queue_size=1)
+rospy.wait_for_service('add_two_number')
+requester = rospy.ServiceProxy('add_two_number', AddTwoNum)
+print("requester type:", type(requester), ", callable?", callable(requester))
 msg = common_msgs()
-rate = rospy.Rate(1)
+rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     msg.timestamp = rospy.get_rostime()
     second = msg.timestamp.secs
     msg.vector = Vector3(x=second%4, y=second%7, z=second%5)
     pub.publish(msg)
-    print "publish:", msg.timestamp.secs%100, msg.vector.x, msg.vector.y, msg.vector.z
+    print("publish:", msg.timestamp.secs%100, msg.vector.x, msg.vector.y, msg.vector.z)
+    if second % 20 == 0:
+        req = AddTwoNumRequest(a=msg.vector.x, b= msg.vector.y/ 2, c= msg.vector.z*2)
+        res = requester(req)
     rate.sleep()
-
